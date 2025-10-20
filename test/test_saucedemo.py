e6097a34eb28d7b1b98dcec05431fe9c603771f1
riki18ar <riki18ar@hotmail.com>
2025-10-20 15:34:26 -0300

    creacion de carpetas, archivos para el proyecto de pre-entrega

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -3,26 +3,25 @@ import pytest
 
 
 
-@pytest.fixture
+#@pytest.fixture
     
-def driver():
+#def driver():
     # configuracion para consultar a selenium web driver.
 
 
-    def test_login():
+    #def test_login():
     # login de usuario con username y password.
     # click en boton login.
     # redireccionar a la pagina de inventario.
     # verificar el titulo de la pagina de inventario.
-    def test_catalogo():
+    #def test_catalogo():
     # login con usuario y password.
     # click en boton login.
     # podamos verificar el titulo, pero del html del catalogo.
     # verificar si existen productos en la pagina y estan visibles.
     # verificar elementos importantes de la pagina.
-    def test_carrito():
+    #def test_carrito():
     # login con usuario y password.
     # click en boton login.
     # redireccionar a la pagina del carrito de compras.
     # comprobar que el carrito aparezcan productos correctamente.
-
