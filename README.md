c065f34f7eb93af41044b1bc794578084666dd1d
clear <riki18ar@gmail.com>
2025-10-20 12:37:44 -0300

    primera  subida de archivos y carpetas para la pre-entrega

---
+++ README.md
@@ -0,0 +1,10 @@
+Propósito del proyecto
+
+Tecnologías utilizadas: 
+1. Python como lenguaje de programacion principal.
+2. Pytest como framework de pruebas unitarias sencillas asi como tambien para pruebas funcionales mas complejas para la ejecucion y generacion de informes de casos de prueba.
+3. Selenium WebDriver para automatizacion
+4. Git y GitHub para control de versiones
+Instrucciones de instalación de dependencias
+
+Comando para ejecutar las pruebas (por ejemplo: pytest -v --html=reporte.html)
---
+++ test/test_saucedemo.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+
+
+@pytest.fixture
+    
+def driver():
+    # configuracion para consultar a selenium web driver.
+
+
+    def test_login():
+    # login de usuario con username y password.
+    # click en boton login.
+    # redireccionar a la pagina de inventario.
+    # verificar el titulo de la pagina de inventario.
+    def test_catalogo():
+    # login con usuario y password.
+    # click en boton login.
+    # podamos verificar el titulo, pero del html del catalogo.
+    # verificar si existen productos en la pagina y estan visibles.
+    # verificar elementos importantes de la pagina.
+    def test_carrito():
+    # login con usuario y password.
+    # click en boton login.
+    # redireccionar a la pagina del carrito de compras.
+    # comprobar que el carrito aparezcan productos correctamente.
+
