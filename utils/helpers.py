f631f76ff444433e48c80ca7e38154251d8d38b5
riki18ar <riki18ar@hotmail.com>
2025-10-21 20:21:40 -0300

    Se agrega carpeta reports y archivo de report.html , cambios en el archivo helpers (espera implicita en el campo password y boton login

--- utils/helpers.py
+++ utils/helpers.py
@@ -11,29 +11,26 @@ URL = "https://www.saucedemo.com/"
 USERNAME = "standard_user"
 PASSWORD = "secret_sauce"
 
-# Configuración del WebDriver para Chrome en modo headless . 
-# def get_driver():
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")
-#     service = Service(ChromeDriverManager().install())
-#     driver = webdriver.Chrome(service=service, options=chrome_options)
-#     driver.implicitly_wait(10)
-#     return driver
 def get_driver():
-    #chrome_options = Options()
-    #chrome_options.add_argument("--headless")
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = CHROME_BINARY_PATH
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     return driver
 def login_saucedemo(driver):
     driver.get(URL)
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, "user-name"))
     ).send_keys(USERNAME)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "password"))
+    ).send_keys(PASSWORD)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "login-button"))
+    ).click()
+    #driver.find_element(By.ID, "user-name").send_keys(USERNAME)
     # driver.find_element(By.ID, "user-name").send_keys(USERNAME)
-    driver.find_element(By.ID, "password").send_keys(PASSWORD)
-    driver.find_element(By.ID, "login-button").click()
+    #driver.find_element(By.ID, "password").send_keys(PASSWORD)
+    # driver.find_element(By.ID, "login-button").click()
     #time.sleep(2)  # Esperar a que la página cargue después del login
