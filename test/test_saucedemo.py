6f5835af00ddb32254ca343e7931a4bc384ee93a
riki18ar <riki18ar@hotmail.com>
2025-10-21 22:10:45 -0300

    Se agrega configuracion al chrome driver para poder exitar el pop up Cambia la contraseña

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 import sys
 import os
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.helpers import login_saucedemo, get_driver
@@ -16,14 +17,17 @@ def test_login(driver):
     assert "/inventory.html" in driver.current_url
     titulo = driver.find_element(By.CSS_SELECTOR,'div.header_secondary_container .title').text
     assert titulo == "Products"   
+    time.sleep(1)
 def test_catalogo(driver):
     login_saucedemo(driver)
     products = driver.find_elements(By.CLASS_NAME, "inventory_list")
     assert len(products) > 0
+    time.sleep(1)
 def test_carrito(driver):
     login_saucedemo(driver)
     products = driver.find_elements(By.CLASS_NAME, "inventory_item")
     assert len(products) > 0 
     products[0].find_element(By.TAG_NAME, "button").click()
     cart_count = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
-    assert cart_count == "1" 
+    assert cart_count == "1"
+    time.sleep(2) 
--- utils/helpers.py
+++ utils/helpers.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager    
 from selenium.webdriver.chrome.service import Service
-#import time
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
 CHROME_BINARY_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 URL = "https://www.saucedemo.com/"
 USERNAME = "standard_user"
@@ -14,23 +15,38 @@ PASSWORD = "secret_sauce"
 def get_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = CHROME_BINARY_PATH
+    chrome_options.add_argument("--disable-save-password-bubble")
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.default_content_setting_values.notifications": 2,
+        "profile.password_manager_leak_detection": False,
+        "profile.default_content_setting_values.popups": 0
+    }
+    chrome_options.add_experimental_option("prefs", prefs)  
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(5)
-    return driver
+    return driver 
 def login_saucedemo(driver):
     driver.get(URL)
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, "user-name"))
     ).send_keys(USERNAME)
+    time.sleep(1)
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.NAME, "password"))
     ).send_keys(PASSWORD)
+    time.sleep(1)
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.NAME, "login-button"))
     ).click()
-    #driver.find_element(By.ID, "user-name").send_keys(USERNAME)
-    # driver.find_element(By.ID, "user-name").send_keys(USERNAME)
-    #driver.find_element(By.ID, "password").send_keys(PASSWORD)
-    # driver.find_element(By.ID, "login-button").click()
-    #time.sleep(2)  # Esperar a que la página cargue después del login
+    time.sleep(2)
+    try:
+        time.sleep(0.5)
+        Alert(driver).accept()
+        print("Alert accepted")
+    except Exception as e:
+        pass
+    time.sleep(2)
